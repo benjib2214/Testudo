1aca85d00488131a4139ca04ef5cefb71578087e
adavydov <sasha.davydov@gmail.com>
2018-04-07 15:59:37 -0400

    Prints Schedule of Classes

--- IndividualWebPages.py
+++ IndividualWebPages.py
@@ -16,47 +16,49 @@ htmls = getData()
 #f = urllib.request.urlopen(url)
 #fp = f.read().strip()
 #print (fp)
-soup = BeautifulSoup(htmls[0][0], "html.parser")
+for row in range(len(htmls)):
+    for col in range(len(htmls[row])):
+        soup = BeautifulSoup(htmls[row][col], "html.parser")
+        
+        
+        #print(courseNums)
+        courses = []
+        courseName = soup.find_all('span', class_ = "course-title")[0].get_text()
+        for course in soup.find_all('div', class_ = "course-id"):
+            courses.append(course.get_text())
+        j = 0
+        maxim = 0
+        for i in range(len(soup.find_all('span', class_ = "section-id"))):
+            professors = soup.find_all('span', class_ = "section-instructor")[i].get_text()
+            print(professors)
+            sections = soup.find_all('span', class_ = "section-id")[i].get_text().strip()
+            print(sections)
+            try:
+                if int(sections) > maxim:
+                    pass
+                elif int(sections) <= maxim:
+                    j += 1
+                maxim = int(sections)
+            except:
+                pass
+            
+            course = courses[j]
+            #days = soup.find_all('span', class_ = "section-days")[i].get_text()
+            #starttime = soup.find_all('span', class_ = "class-start-time")[i].get_text()
+            #endtime = soup.find_all('span', class_ = "class-end-time")[i].get_text()
+            #times = days + " " + starttime + " - " + endtime
+            #print (times)
+            print (course)
+            openseats = soup.find_all('span', class_ = "open-seats-count")[i].get_text()
+            totalseats = soup.find_all('span', class_ = "total-seats-count")[i].get_text()
+            print (totalseats, openseats)
+            #building = soup.find_all('span', class_ = "building-code")[i].get_text()
+            #classroom = soup.find_all('span', class_ = "class-room")[i].get_text()
+            #location = building + " " + classroom
+            #print (location)
+            print ("\n")
 
-
-#print(courseNums)
-courses = []
-courseName = soup.find_all('span', class_ = "course-title")[0].get_text()
-for course in soup.find_all('div', class_ = "course-id"):
-    courses.append(course.get_text())
-j = 0
-maxim = 0
-for i in range(len(soup.find_all('span', class_ = "section-id"))):
-    professors = soup.find_all('span', class_ = "section-instructor")[i].get_text()
-    print(professors)
-    sections = soup.find_all('span', class_ = "section-id")[i].get_text().strip()
-    print(sections)
-    try:
-        if int(sections) > maxim:
-            pass
-        elif int(sections) <= maxim:
-            j += 1
-        maxim = int(sections)
-    except:
-        pass
-    
-    course = courses[j]
-    #days = soup.find_all('span', class_ = "section-days")[i].get_text()
-    #starttime = soup.find_all('span', class_ = "class-start-time")[i].get_text()
-    #endtime = soup.find_all('span', class_ = "class-end-time")[i].get_text()
-    #times = days + " " + starttime + " - " + endtime
-    #print (times)
-    print (course)
-    openseats = soup.find_all('span', class_ = "open-seats-count")[i].get_text()
-    totalseats = soup.find_all('span', class_ = "total-seats-count")[i].get_text()
-    print (totalseats, openseats)
-    #building = soup.find_all('span', class_ = "building-code")[i].get_text()
-    #classroom = soup.find_all('span', class_ = "class-room")[i].get_text()
-    #location = building + " " + classroom
-    #print (location)
-    print ("\n")
-
-
+print ("Finished")
 #print (classes)
 
 
