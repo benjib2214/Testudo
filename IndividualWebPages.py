4ac389dd65a91279004e5d1ded510b08a05d0136
adavydov <sasha.davydov@gmail.com>
2018-04-07 15:07:53 -0400

    Added initial parser for individual class pages

---
+++ IndividualWebPages.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 21:22:39 2018
+
+@author: Alexander Davydov
+"""
+
+from bs4 import BeautifulSoup
+import urllib
+
+def findAll(string,keyword):
+#    print('find k word')
+    numKeyWord=string.count(keyword,0,len(string))
+    startIndexes = [None]*numKeyWord
+    i=0
+    while i<numKeyWord:
+        if i==0:
+            startIndexes[i]=string.find(keyword)
+        else:
+            startIndexes[i]=string.find(keyword,startIndexes[i-1]+len(keyword)+1)
+        i+=1
+    return startIndexes
+
+url = "https://app.testudo.umd.edu/soc/search?courseId=enes100&sectionId=&termId=201808&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
+
+f = urllib.request.urlopen(url)
+fp = f.read().strip()
+#print (fp)
+soup = BeautifulSoup(fp, "html.parser")
+
+
+#print(courseNums)
+courses = []
+courseName = soup.find_all('span', class_ = "course-title")[0].get_text()
+for course in soup.find_all('div', class_ = "course-id"):
+    courses.append(course.get_text())
+j = 0
+maxim = 0
+for i in range(len(soup.find_all('span', class_ = "section-id"))):
+    professors = soup.find_all('span', class_ = "section-instructor")[i].get_text()
+    print(professors)
+    sections = soup.find_all('span', class_ = "section-id")[i].get_text().strip()
+    print(sections)
+    try:
+        if int(sections) > maxim:
+            pass
+        elif int(sections) <= maxim:
+            j += 1
+        maxim = int(sections)
+    except:
+        pass
+    
+    course = courses[j]
+    #days = soup.find_all('span', class_ = "section-days")[i].get_text()
+    #starttime = soup.find_all('span', class_ = "class-start-time")[i].get_text()
+    #endtime = soup.find_all('span', class_ = "class-end-time")[i].get_text()
+    #times = days + " " + starttime + " - " + endtime
+    #print (times)
+    print (course)
+    openseats = soup.find_all('span', class_ = "open-seats-count")[i].get_text()
+    totalseats = soup.find_all('span', class_ = "total-seats-count")[i].get_text()
+    print (totalseats, openseats)
+    #building = soup.find_all('span', class_ = "building-code")[i].get_text()
+    #classroom = soup.find_all('span', class_ = "class-room")[i].get_text()
+    #location = building + " " + classroom
+    #print (location)
+    print ("\n")
+
+
+#print (classes)
+
+
