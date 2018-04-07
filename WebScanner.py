41b1c72473c95c1168112608fa717a1eb4fd5745
benjib2214 <benji.b2214@gmail.com>
2018-04-07 15:11:39 -0400

    added Kevin's WebScabber.py file

---
+++ WebScanner.py
@@ -0,0 +1,38 @@
+from urllib.request import urlopen, Request
+from joblib import Parallel, delayed
+import re
+import time
+
+def parseClass(subUrl, cl):
+	return urlopen(subUrl+'/'+cl[0:7]).read().decode('utf-8')
+
+def findSub(f,match):
+    classHtml = []
+    try:
+        subUrl = 'https://app.testudo.umd.edu/soc/'+match
+        a = urlopen(subUrl)
+       # cookie = a.info()['Set-Cookie']
+        subHtml = a.read().decode('utf-8')
+        #print(subHtml)		
+        #print(sub[7:10])		
+        classes = re.findall('[A-Z]{4}\d{3}\" class=\"course', subHtml)
+        #print(classes)
+        #Parallel(n_jobs=len(classes))(delayed(parseClass)('https://app.testudo.umd.edu/soc/201808/sections?courseIds=', cl) for cl in classes)
+        for cl in classes:
+            classHtml.append(urlopen(subUrl+'/'+cl[0:7]).read().decode('utf-8'))	
+        print('done ', match)
+        return match,classHtml
+    except:
+        print(match)
+        return None
+def main():
+    f = urlopen('https://app.testudo.umd.edu/soc/search?courseId=&sectionId=&termId=201808&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on')
+    #print(f.read())
+    html = f.read().decode('utf-8')
+    #print(html)
+    match = re.findall('\d{6}\/[A-Z]{4}', html)
+    return Parallel(n_jobs=32,backend="threading")(delayed(findSub)(f,match[i]) for i in range(len(match)))
+	
+start = time.time()
+res = main()
+print(time.time()-start)
