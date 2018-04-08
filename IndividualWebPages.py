e300c7c92925c3a84f31eb15d3eb6b0390064d51
adavydov <sasha.davydov@gmail.com>
2018-04-07 23:15:56 -0400

    Commented code

--- IndividualWebPages.py
+++ IndividualWebPages.py
@@ -15,24 +15,29 @@ def extractHTMLData(htmls,classes):
     if htmls == None: 
         return None
     returnInfo = []
+    # htmls contains a 2 dimensional array of html strings and we use BeautifulSoup to make it easier to parse
     for col in range(len(htmls)):
         soup = BeautifulSoup(htmls[col], "html.parser")
         
-        
+        # Take into account honors sections, scholars sections, etc.
         courses = []
+        # Get course title such as Linear Algebra
         courseName = soup.find_all('span', class_ = "course-title")[0].get_text()
+        # Get course abbreviation such as MATH240
         for course in soup.find_all('div', class_ = "course-id"):
             courses.append(course.get_text())
         j = 0
         maxim = 0
+        # check for section numbers
         for i in range(len(soup.find_all('span', class_ = "section-id"))):
+            # get professors for respective sections
             professors = soup.find_all('span', class_ = "section-instructor")[i].get_text()
-            #print(professors)
             sections = soup.find_all('span', class_ = "section-id")[i].get_text().strip()
-            #print(sections)
             try:
+                # continue if section number increases
                 if int(sections) > maxim:
                     pass
+                # go to next course abbreviation (honors sections) if section number decreases
                 elif int(sections) <= maxim:
                     j += 1
                 maxim = int(sections)
@@ -40,21 +45,25 @@ def extractHTMLData(htmls,classes):
                 pass
             
             course = courses[j]
-            #days = soup.find_all('span', class_ = "section-days")[i].get_text()
-            #starttime = soup.find_all('span', class_ = "class-start-time")[i].get_text()
-            #endtime = soup.find_all('span', class_ = "class-end-time")[i].get_text()
-            #times = days + " " + starttime + " - " + endtime
-            #print (times)
-            #print (course)
+            '''days = soup.find_all('span', class_ = "section-days")[i].get_text()
+            starttime = soup.find_all('span', class_ = "class-start-time")[i].get_text()
+            endtime = soup.find_all('span', class_ = "class-end-time")[i].get_text()
+            times = days + " " + starttime + " - " + endtime
+            print (times)
+            print (course)
+            '''
+            # Get number of open seats
             openseats = soup.find_all('span', class_ = "open-seats-count")[i].get_text()
+            # Get total number of seats
             totalseats = soup.find_all('span', class_ = "total-seats-count")[i].get_text()
             returnInfo.append((course,sections,None,professors,openseats,totalseats,None))
-            #print (totalseats, openseats)
-            #building = soup.find_all('span', class_ = "building-code")[i].get_text()
-            #classroom = soup.find_all('span', class_ = "class-room")[i].get_text()
-            #location = building + " " + classroom
-            #print (location)
-            #print ("\n")
+            '''print (totalseats, openseats)
+            building = soup.find_all('span', class_ = "building-code")[i].get_text()
+            classroom = soup.find_all('span', class_ = "class-room")[i].get_text()
+            location = building + " " + classroom
+            print (location)
+            print ("\n")
+            '''
     return returnInfo
 start = time.time()
 htmls = getData()
@@ -63,7 +72,9 @@ print (end - start)
 
 
 classes = AllCourse()
+# Parallelize process for efficiency
 information = Parallel(n_jobs=20)(delayed(extractHTMLData)(htmls[row],classes) for row in range(len(htmls)))
+# Add class information into proper dataframe
 for i in range(len(information)):
     if information[i] == None:
         continue
